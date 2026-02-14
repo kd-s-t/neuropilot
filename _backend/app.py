a4375092d0c6d4cb8c7a7ffe01f524dd51687cd9
Ken Dan Tinio <kendantinio@gmail.com>
2026-02-15 07:01:50 +0800

    lets gooo

--- _backend/app.py
+++ _backend/app.py
@@ -17,7 +17,14 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000", "http://127.0.0.1:3001"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
+        "https://app.theneuropilot.com",
+        "https://theneuropilot.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
